e1dc6ae8f39ada9031d1b79692fb1b9712fbfda8
Kevin <kevin.shehan30@gmail.com>
2024-10-19 15:24:01 +0530

    css custome added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import './App.css'
-import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
-import ProfilePicture from './components/profilepicture/ProfilePicture';
-import styled, { ThemeProvider } from "styled-components";                                                                                                                              
-import {darkTheme} from './utils/Themes';
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from './utils/Themes';
+import {BrowserRouter } from 'react-router-dom';
+
 
 function App() {
-
-
-
   return (
     <>
-      <ThemeProvider>
-Kevin Shehan Perera
-      </ThemeProvider>
+      <ThemeProvider theme={darkTheme}>
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
+      </ThemeProvider >
     </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-// import ThemeSwitcher from './components/ThemeSwitcher';
+
+import { Link as LinkR } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavbarContainer = styled.div`
+background-color:white`;
+const NavLogo = styled(LinkR)``;
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <h2>Alvin</h2>
-      </div>
-      <ul className="nav-links">
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-      {/* <ThemeSwitcher theme={theme} setTheme={setTheme} /> */}
-    </nav>
+    <NavbarContainer className="navbar">
+      <NavLogo>
+        <a>Kevin</a>
+      </NavLogo>
+    </NavbarContainer>
   )
 }
 
