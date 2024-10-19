2b7ec60e406b21ad16e366da592648ed225b3fa5
Kevin <kevin.shehan30@gmail.com>
2024-10-19 17:51:31 +0530

    Navbar css added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,16 @@ import { Link as LinkR } from 'react-router-dom';
 import styled from 'styled-components';
 
 const NavbarContainer = styled.div`
-background-color:white`;
+background-color:${({theme})=>theme.bg};
+height:80px;
+display:flex;
+align-items:center;
+justify-content:center;
+font-size:1rem;
+position:sticky';
+top:0;
+z-index:10;`;
+
 const NavLogo = styled(LinkR)``;
 
 const Navbar = () => {
